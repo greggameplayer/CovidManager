10c7301e8058a51b901b95a2c1e4701d1b12cbc2
victor.marit <victormarit.95@gmail.com>
2021-03-26 23:53:58 +0100

    Update xml file details_vaccine

--- app/src/main/java/com/epsi/covidmanager/controller/DetailsVaccine.java
+++ app/src/main/java/com/epsi/covidmanager/controller/DetailsVaccine.java
@@ -2,13 +2,13 @@ package com.epsi.covidmanager.controller;
 
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.epsi.covidmanager.R;
 import com.epsi.covidmanager.model.beans.Vaccine;
-import com.epsi.covidmanager.model.mongo.Slot;
 
 
 
@@ -16,7 +16,8 @@ public class DetailsVaccine extends AppCompatActivity {
 
     public final static String VACCINE_KEY = "VACCINE_KEY";
 
-    private TextView tv_detail_vaccine_name;
+    private Button bt_details_vaccine_add;
+    private TextView tv_details_vaccine_quantity_prev, tv_details_vaccine_quantity_remain, tv_details_vaccine_quantity, tv_detail_vaccine_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +27,15 @@ public class DetailsVaccine extends AppCompatActivity {
         Vaccine vaccine = (Vaccine) getIntent().getExtras().getSerializable(VACCINE_KEY);
 
         tv_detail_vaccine_name = findViewById(R.id.tv_detail_vaccine_name);
+        tv_details_vaccine_quantity_prev = findViewById(R.id.tv_details_vaccine_quantity_prev);
+        tv_details_vaccine_quantity_remain = findViewById(R.id.tv_details_vaccine_quantity_remain);
+        tv_details_vaccine_quantity = findViewById(R.id.tv_details_vaccine_quantity);
+
+
         tv_detail_vaccine_name.setText(vaccine.getName());
-
-
+        tv_details_vaccine_quantity_prev.setText(vaccine.getNb()+"");
+        tv_details_vaccine_quantity_remain.setText("Réfléchir sur la notion en groupe");
+        tv_details_vaccine_quantity.setText(vaccine.getNbPrev()+"");
 
 
     }
